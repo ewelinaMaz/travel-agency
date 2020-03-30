1068e55aea463cc689eea73306a87438bec9de11
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-03-30 13:02:41 +0200

    18.2 Fix issue with Page title

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 //import styles from './OrderForm';
 import {Row, Col} from 'react-flexbox-grid';
 import PropTypes from 'prop-types';
-import PageTitle from '../../common/PageTitle/PageTitle';
+
 
 const OrderForm = props => (
 
@@ -11,7 +11,6 @@ const OrderForm = props => (
   
   <Row>
     <Col xs={12}>
-      <PageTitle text='Trip options' />
       <OrderSummary cost={props.tripCost} options={props.options} />
     </Col>
   </Row>
