947b7dfe472bc7d0937b752619ff3067415a8ffd
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-04-01 15:54:11 +0200

    Add Option dropdown and Icon

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,15 +3,19 @@ import React from 'react';
 //import styles from './OrderForm';
 import {Row, Col} from 'react-flexbox-grid';
 import PropTypes from 'prop-types';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
-
-const OrderForm = props => (
-
-
+const OrderForm = ({options, tripCost, setOrderOption}) => (
   
   <Row>
+    {pricing.map(option => (
+      <Col key={option.id} md={4}>
+        <OrderOption {...option} currentValue = {options[option.id]} setOrderOption ={setOrderOption}/>
+      </Col>
+    ))}
     <Col xs={12}>
-      <OrderSummary cost={props.tripCost} options={props.options} />
+      <OrderSummary cost={tripCost} options={options} />
     </Col>
   </Row>
 
@@ -20,6 +24,7 @@ const OrderForm = props => (
 OrderForm.propTypes = {
   tripCost: PropTypes.node,
   options: PropTypes.object,
+  setOrderOption: PropTypes.func,
 };
 
 export default OrderForm;
